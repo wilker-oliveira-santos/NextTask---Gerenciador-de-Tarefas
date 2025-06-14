6e804c8b867ba0984733401569ea7f6c07b1cde0
gabriel-gbl <136861664+gabriel-gbl@users.noreply.github.com>
2025-06-14 12:41:20 -0300

    parte de requisição faltando update, acrescentei notificação, pagina sobre nós e icone no link

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 from flask import Flask
 from routes.login import login_route
 from routes.cad_tarefas import cad_route
-
+from routes.cad_tarefas import del_route
 app = Flask(__name__)
 
 app.register_blueprint(login_route)
 app.register_blueprint(cad_route)
+app.register_blueprint(del_route)
 
 app.run(debug=True)
--- routes/cad_tarefas.py
+++ routes/cad_tarefas.py
@@ -1,27 +1,45 @@
 from flask import Blueprint,render_template,request
+from plyer import notification
 
 cad_route  = Blueprint('cadastrar', __name__)
+del_route  = Blueprint('deletar', __name__)
+up_route  = Blueprint('update', __name__)
 
-tarefas_nome = []
-tarefas_data = []
-tarefa_nivel = []
+tarefas = []
 
 @cad_route.route('/cadastro', methods=['POST'])
 def cad_tarefas():
     task = request.form['task']
     data = request.form['data_task']
-    nivel = request.form['nivel']
+    nivel = request.form['nivel'].upper()
 
-    if(nivel=="moderado"):
-        tarefa_nivel.append("Moderado")
-    elif(nivel=="urgente"):
-        tarefa_nivel.append("Urgente")
-    else:
-        tarefa_nivel.append("Baixo")
-    
+    tarefas.append({
+        "tarefa": task,
+        "data": data,
+        "nivel": nivel
+    })
 
-    tarefas_nome.append(task)
-    tarefas_data.append(data)
-    return render_template('home.html', tarefas=tarefas_nome, data=tarefas_data, nivel=tarefa_nivel) 
+    return render_template('home.html', tarefas=tarefas) 
+
+@del_route.route('/delete', methods=['POST'])
+def del_tarefa():
+    id_tarefa = int(request.form['id'])
+    tarefas.pop(id_tarefa)
+
+    notification.notify(
+        title='Next Task',
+        message='Tarefa excluída com Sucesso!',
+        app_icon='./static/images/favicon.ico', 
+        timeout=0.1 
+    )
+
+    return render_template('home.html', tarefas=tarefas)
+
+@up_route.route('/update', methods=['POST'])
+def up_tarefa():
+    id_tarefa = int(request.form['id'])
+    tarefas.pop(id_tarefa)
+
+    return render_template('home.html', tarefas=tarefas)
     
 
