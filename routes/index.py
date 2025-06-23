26e4cf879e90a73ec469f113380e484c400e808c
wilker-oliveira-santos <wilkeroliveirasantos00@gmail.com>
2025-06-22 22:58:21 -0300

    Projeto Next-Task Finalizado

--- routes/index.py
+++ routes/index.py
@@ -9,15 +9,3 @@ def index():
 @index_route.route('/home')
 def home():
     return render_template('home.html')
-
-"""@login_route.route('/home', methods=['POST'])
-def receber_dados():
-
-    email = request.form['email']
-    nome = request.form['nome']
-
-
-    cad_email.append(email)
-    return render_template('home.html', email=email, list=list , nome=nome, cad_email=cad_email)"""
-
-
